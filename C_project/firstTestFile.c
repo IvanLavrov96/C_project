c2ae7e9cde35acf5e0f219a2d51ccdede8563230
Ivan Lavrov <vano4411@gmail.com>
2021-12-27 13:48:21 +0200

    The test file

---
+++ C_project/firstTestFile.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*
+PLEASE be awate that im using x86 (32bit) compiler, so all the bin files from this project shuold be compiled with x86 !!!
+*/
+void main() {
+
+	printf("hello kiril");
+	//hello
+}
